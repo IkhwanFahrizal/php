6b51cc230fed78f7e49b3b8ceaf6791205476551
IkhwanFahrizal <alfaikhwan@gmail.com>
2023-05-03 19:33:19 +0700

    update looping ganjil genap

---
+++ looping_ganjil-genap.php
@@ -0,0 +1,12 @@
+<?php
+    echo "<h1><center>Looping angka ganjil - genap</h1>";
+
+    for ($i = 1; $i <= 9; $i++) {
+        if ($i % 2 == 0) {
+            echo "$i adalah bilangan genap" . "<br>";
+        }   
+        else {
+            echo "$i adalah bilangan ganjil" . "<br>";
+    }
+}
+?>
