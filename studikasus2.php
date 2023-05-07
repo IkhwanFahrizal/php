1f1a0a6396379189bfbb447c4bc9436eb56b00cb
IkhwanFahrizal <alfaikhwan@gmail.com>
2023-05-07 23:11:02 +0700

    update coomit study

--- studikasus2.php
+++ studikasus2.php
@@ -2,60 +2,111 @@
 <html>
     <head>
         <title>Studi Kasus</title>
-    </head>
-<body>
+        <style type="text/css">
+        
+        body {
+			margin: 0;
+			padding: 0;
+		}
+		
+		table {
+			border-collapse: collapse;
+			margin: 0 auto;
+			border: 2px solid gray;
+			width: 75%;	
+		}
 
-<?php
-    $json_string = file_get_contents("data.json");
-    $json = json_decode($json_string,true);
-?>
- 
-<table border="1" style="width: 1000px;" align="center">
-        <tr>
-            <th><b>No</b></th>
-            <th><b>Nama</b></th>
-            <th><b>Kelas</b></th>
-            <th><b>Alamat</b></th>
-            <th><b>Tanggal Lahir</b></th>
-            <th><b>Umur</b></th>
-            <th><b>Nilai</b></th>
-            <th><b>Hasil</b></th>
-        </tr>  
-        <?php 
-        $no = 1; 
-        foreach ($json as $value) { ?>  
-            <tr>
-                <td><?php echo $no ?></td>
-                <td><?php echo $value["nama"] ?></td>
-                <td><?php echo $value["kelas"] ?></td>
-                <td><?php echo $value["alamat"] ?></td>
-                <td><?php echo $value["tanggal_lahir"] ?></td>
-                <td><?php echo $value["umur"] ?></td>
-                <td><?php echo $value["nilai"] ?></td>
-                <td>
-                    <?php
-                        switch ($value["nilai"]) {
-                            case $value["nilai"] >= 90 && $value["nilai"] <= 100:
-                                echo "A";
-                                break;
-                            case $value["nilai"] >= 80 && $value["nilai"] < 90:
-                                echo "B";                                
-                                break;
-                            case $value["nilai"] >= 70 && $value["nilai"] < 80:
-                                echo "C";                                
-                                break;
-                            case $value["nilai"] >= 0 && $value["nilai"] < 70:
-                                echo "D";                                
-                                break;
-                            break;
-                        }           
-                    ?>
-                </td>
-            </tr>
-        <?php  
-            $no++;
-        }
-        ?>      
-   </table>
-</body>
+		th {
+			background-color: #cfceca;
+			color: black;
+			padding: 8px;
+			text-align: center;
+			border: 2px solid gray;
+		}
+
+		td {
+			padding: 8px;
+			text-align: left;
+			border: 2px solid gray;
+		}
+
+		h2 {	
+			background-color: #e6b800;
+			margin-top: 0px;
+			margin-bottom: 50px;
+			padding: 16px;
+            text-align: center;
+		}
+        </style>
+    </head>
+
+    <body>
+        <h2> Daftar Nilai </h2>
+	    <table>
+		    <tr>
+			    <th>No</th>
+			    <th>Nama</th>
+			    <th>Tanggal Lahir</th>
+			    <th>Umur</th>
+			    <th>Alamat</th>
+			    <th>Kelas</th>
+			    <th>Nilai</th>
+			    <th>Hasil</th>
+		    </tr>
+
+		    <?php 
+			    $data = file_get_contents('data.json');
+			    $json = json_decode($data, true);
+
+			    $arraybulan = array(
+				    1 => 'Januari',
+				    2 => 'Februari',
+				    3 => 'Maret',
+				    4 => 'April',
+				    5 => 'Mei',
+				    6 => 'Juni',
+				    7 => 'Juli',
+				    8 => 'Augustus',
+				    9 => 'September',
+				    10 => 'Oktober',
+				    11 => 'November',
+				    12 => 'Desember'
+				    );
+			
+			    $nomor = 1;
+			    foreach ($json as $data) {
+				    $jumlah_data = count($data);
+				    $tanggal = date('d', strtotime($data['tanggal_lahir']));
+				    $bulan = $arraybulan[date('n', strtotime($data['tanggal_lahir']))];
+				    $tahun = date('Y', strtotime($data['tanggal_lahir']));
+				    $tanggal_lahir = date_create($data['tanggal_lahir']);
+				    $sekarang = date_create();
+				    $umur = date_diff($tanggal_lahir, $sekarang)->y;
+                    
+				    if ($data['nilai'] >= 90) {
+					    $hasil = 'A';
+				    } elseif ($data['nilai'] >= 80) {
+					    $hasil = 'B';
+				    } elseif ($data['nilai'] >= 70) {
+					    $hasil = 'C';
+				    } else {
+					    $hasil = 'D';
+				    }
+		    ?>
+		    <tr>
+			    <td><?php echo $nomor ?></td>
+			    <td><?php echo $data['nama'] ?></td>
+			    <td><?php echo $tanggal .' '. $bulan .' '. $tahun;?></td>
+			    <td><?php echo $umur . " tahun" ?></td>
+			    <td><?php echo $data['alamat'] ?></td>
+			    <td><?php echo $data['kelas'] ?></td>
+			    <td><?php echo $data['nilai'] ?></td>
+			    <td><?php echo $hasil ?></td>
+		    </tr>
+		    <?php 
+			    $nomor++;
+			    } 
+		    ?>
+	    </table>
+    </body>
 </html>
